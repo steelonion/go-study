b21355bcb125be536bc1fd99857cddee19aaa565
steelonion <steelonion@foxmail.com>
2023-12-06 23:17:53 +0800

    添加varb

--- helloworld.go
+++ helloworld.go
@@ -22,9 +22,32 @@ func main() {
 	// 设置处理函数
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/count", countHandler)
+	http.HandleFunc("/addvarb", addVarbHandler)
 
 	// 启动HTTP服务器并监听端口
 	http.ListenAndServe(":8080", nil)
+}
+
+func addVarbHandler(w http.ResponseWriter, r *http.Request) {
+	// 使用互斥锁保护计数器的并发访问
+	countMu.Lock()
+	defer countMu.Unlock()
+
+	// 添加新的varb
+	testvarbList = append(testvarbList, testvarb{StringField: "test", IntField: count})
+
+	// 将消息转换为JSON格式
+	jsonData, err := json.Marshal(testvarbList)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// 设置响应头为JSON格式
+	w.Header().Set("Content-Type", "application/json")
+
+	// 发送JSON响应
+	w.Write(jsonData)
 }
 
 func countHandler(w http.ResponseWriter, r *http.Request) {
---
+++ myvar.go
@@ -0,0 +1,9 @@
+package main
+
+// 定义类型 testvarb
+type testvarb struct {
+	StringField string
+	IntField    int
+}
+
+var testvarbList []testvarb
